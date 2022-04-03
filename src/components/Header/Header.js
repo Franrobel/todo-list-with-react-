1fb6b65e5da529946a6243f18ae9d60500500f3a
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2022-04-03 22:47:53 +0200

    some shadow to the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
 
-        <div className='flex flex-col items-center py-2 header-container'>
+        <div className='flex flex-col items-center py-2 header-container shadow-lg'>
             <img src="https://openclipart.org/image/800px/16976" width={"40px"} />
             <h1 className='text-2xl'>To do list</h1>
             <p >Create your tasks</p>
