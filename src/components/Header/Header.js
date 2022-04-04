b7fce887e2fd0b27574869229ae1d156ad7f9558
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2022-04-04 23:19:04 +0200

    organizing header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Header = () => {
+
     return (
         <div className='flex flex-col items-center py-2 header-container shadow-lg'>
-            <img src="https://openclipart.org/image/800px/16976" width={"40px"} />
+            <img src="https://openclipart.org/image/800px/16976" width={"40px"} alt='logo of app'/>
             <h1 className='text-2xl'>To do list</h1>
             <p >Create your tasks</p>
         </div>
